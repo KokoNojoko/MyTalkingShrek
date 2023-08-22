41f9d377b3e29eb26d656924595a7d4acaa3e6ca
liamerens <ST10085392@vcconnect.edu.za>
2023-08-22 10:58:32 +0200

    update, taught koko about setting image resource through CODE!!! WOW.

--- app/src/main/java/com/example/mytalkingshrek/PetFragment.kt
+++ app/src/main/java/com/example/mytalkingshrek/PetFragment.kt
@@ -1,39 +1,46 @@
-package com.example.mytalkingshrek
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.lifecycle.ViewModelProvider
+import com.example.mytalkingshrek.R
 
 class PetFragment : Fragment() {
-
-    private lateinit var frameLayout: FrameLayout
-    private lateinit var button: ImageButton
+    private lateinit var progressViewModel: ProgressViewModel
+    private lateinit var petMoodImageView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_pet, container, false)
-        frameLayout = layout.findViewById(R.id.frameLayout)
-        button = layout.findViewById(R.id.Food)
-        button.setOnClickListener{
-            val imageView = ImageView(requireContext())
-            imageView.setImageResource(R.drawable.shrek_eating)
+        val rootView = inflater.inflate(R.layout.fragment_pet, container, false)
+        progressViewModel = ViewModelProvider(requireActivity()).get(ProgressViewModel::class.java)
+        petMoodImageView = rootView.findViewById(R.id.imgStart1)
 
-            val layoutParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT
-            )
-            imageView.layoutParams = layoutParams
+        val feedButton: ImageButton = rootView.findViewById(R.id.Food)
+        val waterButton: ImageButton = rootView.findViewById(R.id.Drink)
+        val washButton: ImageButton = rootView.findViewById(R.id.Wash)
 
-            frameLayout.addView(imageView)
+        feedButton.setOnClickListener {
+            progressViewModel.updateFoodProgress(10)
+            petMoodImageView.setImageResource(R.drawable.shrek_eating)
         }
-        return layout
+
+        waterButton.setOnClickListener {
+            progressViewModel.updateThirstProgress(10)
+            petMoodImageView.setImageResource(R.drawable.shrek_drinking)
+
+        }
+
+        washButton.setOnClickListener {
+            progressViewModel.updateCleanProgress(10)
+            petMoodImageView.setImageResource(R.drawable.shrek_bathing)
+
+        }
+
+        return rootView
     }
 }
